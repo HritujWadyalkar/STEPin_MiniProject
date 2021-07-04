d29d35b1c6cee91573dbb606c6b0f6f49b9f3c4a
Athul Thomas <thomasathul@gmail.com>
2021-07-05 02:32:28 +0530

    Adding implementation files
    -Digital Clock source code
    -Unity files

---
+++ 3_Implementation/src/digitalClock.c
@@ -0,0 +1,55 @@
+#include "prototypes.h"
+#include <time.h>
+
+error_t digitalClock()
+{
+    time_t s;
+    struct tm *current_time;
+
+    // time in seconds
+    s = time(NULL);
+
+    // to get current time
+    current_time = localtime(&s);
+    if (current_time == NULL || current_time != localtime(&s))
+        return TIMEERROR;
+    printf("\n");
+    int cooldown = 10;
+    while (cooldown--)
+    {
+        //increase second
+        current_time->tm_sec++;
+        //update hour, minute and second
+        if (current_time->tm_sec == 60)
+        {
+            current_time->tm_min += 1;
+            current_time->tm_sec = 0;
+        }
+        if (current_time->tm_min == 60)
+        {
+            current_time->tm_hour += 1;
+            current_time->tm_min = 0;
+        }
+        if (current_time->tm_hour == 24)
+        {
+            current_time->tm_hour = 0;
+            current_time->tm_min = 0;
+            current_time->tm_sec = 0;
+        }
+
+        // print time in minutes,
+        // hours and seconds
+
+        // print time in minutes,
+
+        printf("\r%02d:%02d:%02d",
+               current_time->tm_hour,
+               current_time->tm_min,
+               current_time->tm_sec);
+        fflush(stdout);
+
+        sleep(1);
+    }
+
+    return SUCCESS;
+}
